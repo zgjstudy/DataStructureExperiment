1a4b0a097aebf5cae41768e5dc765d4078f7287b
zgjstudy <981225888@qq.com>
2018-10-18 12:13:47 +0800

    实现了一些函数

--- First/LList.h
+++ First/LList.h
@@ -17,70 +17,138 @@ class LList : public List<T>
 private:
 	Link<T>* header;	//头哨兵
 	Link<T>* tailer;	//尾哨兵
-	Link<T>* curr;	//当前节点
-	int _size;		//规模
+	Link<T>* curr;		//当前节点
+	int _size;			//规模
 
 protected:
 	void init()
 	{
 		header = new Link<T>;
 		tailer = new Link<T>;
-		header->next = tailer;
-		tailer->prev = header;
+		header->next = tailer; header->prev = nullptr;
+		tailer->prev = header; tailer->next = nullptr;
+		_size = 0;
+	}
+	
+public:
+	LList(int size = DefaultSize)
+	{
+		init();
 	}
 
-	void removeAll();
-
-public:
-	LList(int size = DefaultSize);
-
-	~LList();
+	~LList()
+	{
+		clear();
+		delete header;
+		delete tailer;
+	}
 
 	//clear
-	void clear();
+	void clear()
+	{
+		curr = header->next;
+		while (curr != tailer)
+		{
+			curr = curr->next;
+			delete curr->prev;
+		}
+		_size = 0;
+	}
 
 	//move to start
-	void moveToStart();
+	void moveToStart()
+	{
+		curr = header->next;
+	}
 
 	//move to end
-	void moveToEnd();
+	void moveToEnd()
+	{
+		curr = tailer->prev;
+	}
 
 	//curr point to predecessor
-	void pred();
+	void pred()
+	{
+		curr = curr->next;
+	}
 
 	//curr point to successor
-	void succ();
+	void succ()
+	{
+		curr = curr->prev;
+	}
 
 	//insert as successor
-	void insert(const T&);
+	void insert(const T& it)
+	{
+		Link<T>* temp = curr;
+		curr = curr->next;
+		curr->prev = temp->next = new Link<T>(it, temp, curr);
+		curr = curr->prev;
+		_size++;
+	}
 
 	//insert as predecessor
-	void insertAsPred(const T&);
+	void insertAsPred(const T& it)
+	{
+		Link<T>* temp = curr;
+		curr = curr->prev;
+		curr->next = temp->prev = new Link<T>(it, curr, temp);
+		curr = curr->next;
+		_size++;
+	}
 
 	//insert as last
-	void append(const T&);
+	void append(const T& it)
+	{
+		Link<T>* temp = tailer->prev;
+		temp->next = tailer->prev = new Link<T>(it, temp, tailer);
+		_size++;
+	}
 
 	//insert as first
-	void insertAsFirst(const T&);
+	void insertAsFirst(const T& it)
+	{
+		Link<T>* temp = header->next;
+		temp->prev = header->next = new Link<T>(it, header, temp);
+		_size++;
+	}
 
 	//remove curr node, then curr point to successor
 	//return curr->element
-	T remove();
+	T remove()
+	{
+		T tempE = curr->element;
+		Link<T>* temp = curr;
+		curr = curr->next;
+		temp->next = curr;
+		curr->prev = temp->prev;
+		delete temp;
+		_size--;
+		return tempE;
+	}
 
 	//remove pth node
 	T remove(int p);
 
+	//return the size of the list
+	virtual int length() const
+	{
+		return _size;
+	}
 
-	virtual int length() const;
-
-
+	//return current position
 	virtual int currPos() const;
 
-
+	//move to position
 	virtual void moveToPos(int pos);
 
-
-	virtual const T& getValue() const;
+	//return current element
+	virtual const T& getValue() const
+	{
+		return curr->element;
+	}
 
 };
 
